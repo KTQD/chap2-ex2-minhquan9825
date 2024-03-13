d03b0401cd035e6d9dd6a2482a69a44a6dcbee63
minhquan9825 <163239513+minhquan9825@users.noreply.github.com>
2024-03-13 15:20:13 +0700

    Update main.c

--- main.c
+++ main.c
@@ -4,11 +4,11 @@ int main() {
      float height, width, perimeter;
 
     printf("Enter the height and width of the rectangle: ");
-    scanf("%f" %f", &height, &width);
+    scanf("%d" %d", &height, &width);
 
     perimeter = 2 * (height + width);
 
-    printf("The perimeter of the rectangle is: %.2f\n", perimeter);
+    printf("The perimeter of the rectangle is: %.2d\n", perimeter);
 
     return 0;
 }
